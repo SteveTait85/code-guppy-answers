54dc54f21b46e22e448994e67b79f13356f487c4
Steve Tait <steve.tait@theiconic.com.au>
2020-01-06 20:35:15 +1100

    completed challenge 2

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // Coding challenge #1: Print numbers from 1 to 10
-console.log("==============Challenge 1=================")
+console.log("==============Challenge 1=================");
 
 for (let i = 1; i <= 10; i++) {
 	console.log(i);
 }
-console.log("==============Challenge 2=================")
+console.log("==============Challenge 2=================");
+
+for (let i = 1; i <= 100; i += 2) {
+	console.log(i);
+
+}
+console.log("==============Challenge 3=================");
